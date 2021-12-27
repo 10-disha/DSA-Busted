b8b0cc856df1cdda8bfb6ba973db3024566b02d8
10-disha <jayswaldisha5@gmail.com>
2021-12-27 09:32:20 +0530

    lecture 5

---
+++ Lecture5/continue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "disha" << endl;
+        cout << "jayswal" << endl;
+        continue;
+        cout << "rajubhai" << endl;
+    }
+}
---
+++ Lecture5/primeFor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "number is:";
+    cin >> n;
+
+    bool isPrime = 1;
+    for (int i = 2; i < n; i++)
+    {
+    }
+}
