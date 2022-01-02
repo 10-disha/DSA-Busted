ca395d92f938a4aae596461163251e10ca3ab564
10-disha <jayswaldisha5@gmail.com>
2022-01-02 10:58:14 +0530

    inline functions

--- Lecture30/inlineFunction.cpp
+++ Lecture30/inlineFunction.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void func(int a, int b)
-{
-    a++;
-    b++;
-    cout << a << " " << b << endl;
-}
+// void func(int a, int b)
+// {
+//     a++;
+//     b++;
+//     cout << a << " " << b << endl;
+// }
 
 int main()
 {
     int a = 1, b = 2;
-    func(a, b);
+    // func(a, b);
+
+    int ans = 0;
+
+    // method 1
+    //  if(a>b){
+    //      ans=a;
+    //  }
+    //  else{
+    //      ans=b;
+    //  }
+
+    // method 2
+
+    ans = (a > b) ? a : b;
 
     return 0;
 }
