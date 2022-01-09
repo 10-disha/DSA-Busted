7789e285189c5053b06c590f8cad498cdad1c7be
10-disha <jayswaldisha5@gmail.com>
2022-01-09 17:02:25 +0530

    mergesort

---
+++ Lecture35/mergeSort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void mergeSort(int *arr, int s, int e)
+{
+    // base case
+    if (s > e)
+    {
+        return;
+    }
+
+    // now left part ko solve karna hai.
+}
+
+int main()
+{
+    int arr[5] = {2, 3, 44, 1, 6};
+    int n = 5;
+
+    mergeSort(arr, 0, n - 1);
+
+    return 0;
+}
