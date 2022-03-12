09f70d981c293125c9435612a8b30b6bc68daf27
10-disha <jayswaldisha5@gmail.com>
2022-03-12 14:56:49 +0530

    lect 36

---
+++ Lecture36/quicSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {2, 4, 1, 6, 9};
+    int n = 5;
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
