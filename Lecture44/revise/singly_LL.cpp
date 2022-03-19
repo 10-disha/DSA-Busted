3fc0a713eea0954d94c240905163213f2757303f
10-disha <jayswaldisha5@gmail.com>
2022-03-19 18:00:41 +0530

    Linked List day 1 complete

--- Lecture44/revise/singly_LL.cpp
+++ Lecture44/revise/singly_LL.cpp
@@ -14,6 +14,19 @@ public:
         this->data = data;
         this->next = NULL;
     }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "memory is free for node with data" << value << endl;
+    }
 };
 
 void insertAtHead(Node *&head, int d)
@@ -38,6 +51,77 @@ void print(Node *&head)
     cout << endl;
 }
 
+void insertAtTail(Node *&tail, int d)
+{
+    // new node create
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = tail->next;
+}
+
+void InsertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // inserting at last position
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    // creating a node for d
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+
+void deleteNode(int position, Node *&head)
+{
+    // deleting first or start node
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+
+        // memory free start node ki kr do
+        temp->next = NULL;
+        delete temp;
+    }
+
+    else
+    {
+        // deleting any middle node or last node
+        Node *curr = head;
+        Node *prev = NULL;
+
+        int cnt = 1;
+        while (cnt <= position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
 int main()
 {
     // create a new node
@@ -47,10 +131,20 @@ int main()
 
     // head pointed to node1
     Node *head = node1;
+    Node *tail = node1;
     print(head);
 
-    insertAtHead(head, 12);
+    insertAtTail(tail, 12);
 
+    print(head);
+
+    InsertAtPosition(tail, head, 3, 22);
+    print(head);
+
+    cout << "head" << head->data << endl;
+    cout << "tail" << tail->data << endl;
+
+    deleteNode(1, head);
     print(head);
 
     return 0;
